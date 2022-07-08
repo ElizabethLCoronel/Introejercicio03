22d11e20e93382206ab4d9f54c953e539efd8889
Lizzy <elizabethlpz@live.com.mx>
2022-07-08 18:54:26 -0500

    Tercer ejercicio

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona ();
+
+        persona.setEdad(54);
+        persona.setNombre("Alejandro");
+        persona.setTelefono("55-3126-0088");
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad (int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre (){
+        return this.nombre;
+    }
+    public void setTelefono (String telefono){
+        this.telefono = telefono;
+    }
+    public String getTelefono (){
+        return this.telefono;
+    }
+
+}
